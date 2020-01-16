e0aef9133b516bae53810742e320cd58368509ac
Jonathan Truong <jjtruong90@gmail.com>
2020-01-15 18:13:48 -0800

    Updates files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('bacon cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('impossible burger');
